1a257ee1ac09407bdf71ac3f379e6197841a701f
DESKTOP-FEURCKB\yyt_c <liutao910612@126.com>
2020-05-31 10:26:50 +0800

    Add log and BulkRequest

--- springboot_elasticsearch/src/main/java/com/liutao/es/controller/OrderController.java
+++ springboot_elasticsearch/src/main/java/com/liutao/es/controller/OrderController.java
@@ -6,17 +6,22 @@ import com.liutao.es.vo.Order;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author:LIUTAO
@@ -50,7 +55,7 @@ public class OrderController {
             request.source(source, XContentType.JSON);
             try {
                 result = restClient.index(request, RequestOptions.DEFAULT);
-                System.out.println(result);
+                log.debug("create order ,result : {}",result);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -79,7 +84,7 @@ public class OrderController {
             request.id(id.toString());
             try {
                 result = restClient.get(request, RequestOptions.DEFAULT);
-                log.debug("the response is : {}",result);
+                log.debug("the response is : {}", result);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -93,5 +98,33 @@ public class OrderController {
         return EsUtil.processGetResponse(result);
     }
 
+    /**
+     * Batch create orders
+     *
+     * @param orders
+     * @return
+     */
+    @ApiOperation(value = "Create orders")
+    @PostMapping("orders")
+    public Map<String,Object> createOrders(@RequestBody List<Order> orders) {
+        BulkRequest request = new BulkRequest();
+        request.timeout(TimeValue.timeValueMillis(2));
 
+        //Note: the different type request can be put in the BulkRequest, Forx example : DeleteRequest , UpdateRequest
+        for (Order order : orders) {
+            IndexRequest indexRequest = new IndexRequest(Order.class.getSimpleName().toLowerCase())
+                    .id(order.getId().toString());
+            indexRequest.source(JSON.toJSONString(order), XContentType.JSON);
+            request.add(indexRequest);
+        }
+        BulkResponse bulkResponse = null;
+        try {
+            bulkResponse = restClient.bulk(request, RequestOptions.DEFAULT);
+            restClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return EsUtil.processBulkRequest(bulkResponse);
+    }
 }
--- springboot_elasticsearch/src/main/java/com/liutao/es/util/EsUtil.java
+++ springboot_elasticsearch/src/main/java/com/liutao/es/util/EsUtil.java
@@ -2,9 +2,16 @@ package com.liutao.es.util;
 
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateResponse;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  * @Author:LIUTAO
  * @Description:
@@ -14,19 +21,47 @@ import org.elasticsearch.action.index.IndexResponse;
 @Slf4j
 public class EsUtil {
 
-    private void processIndexResponse(IndexResponse indexResponse){
+    private void processIndexResponse(IndexResponse indexResponse) {
 
     }
 
-    public static Object processGetResponse(GetResponse getResponse){
+    public static Object processGetResponse(GetResponse getResponse) {
         String index = getResponse.getIndex();
         String id = getResponse.getId();
-        log.debug("index is {} , id is {}",index,id);
-        if(getResponse.isExists()){
+        log.debug("index is {} , id is {}", index, id);
+        if (getResponse.isExists()) {
             String source = getResponse.getSourceAsString();
             return JSON.parse(source);
-        }else{
+        } else {
             return null;
         }
+    }
+
+    public static Map<String,Object> processBulkRequest(BulkResponse bulkResponse) {
+        if (bulkResponse == null) {
+            return null;
+        }
+
+        Map<String,Object> responseMap = new HashMap();
+        for (BulkItemResponse bulkItemResponse : bulkResponse.getItems()) {
+            DocWriteResponse itemResponse = bulkItemResponse.getResponse();
+            switch (bulkItemResponse.getOpType()){
+                case INDEX:
+
+                case CREATE:
+                    IndexResponse indexResponse = (IndexResponse)itemResponse;
+                    responseMap.put("CREATE",indexResponse);
+                    break;
+                case UPDATE:
+                    UpdateResponse updateResponse = (UpdateResponse)itemResponse;
+                    responseMap.put("UPDATE",updateResponse);
+                    break;
+                case DELETE:
+                    DeleteResponse deleteResponse = (DeleteResponse)itemResponse;
+                    responseMap.put("DELETE",deleteResponse);
+                    break;
+            }
+        }
+        return responseMap;
     }
 }
