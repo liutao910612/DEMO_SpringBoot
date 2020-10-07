7947b63b3b488f0df9ecf233952ae7296a7fcc93
alvinl <alvinl@synnex.com>
2020-10-07 17:58:39 +0800

    Add function of getting cache

--- springboot_cache/src/main/java/com/liutao/cache/manager/LocalCacheProviderImpl.java
+++ springboot_cache/src/main/java/com/liutao/cache/manager/LocalCacheProviderImpl.java
@@ -6,13 +6,19 @@ import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
 
 /**
  * Local cache by Guava
+ *
  * @author alvinl
  * @date 09/29/20 16:57
  **/
@@ -23,8 +29,9 @@ public class LocalCacheProviderImpl implements CacheProviderService {
 
     private static Map<String, Cache<String, Object>> _cacheMap = Maps.newConcurrentMap();
 
-    private static final int CACHE_MAXIMUM_SIZE = 10000;
-    private static final int CACHE_MINUTE = 60000;
+    private static final Long CACHE_MAXIMUM_SIZE = 10000L;
+    private static final Long CACHE_MINUTE = 60000L;
+    private static Lock lock = new ReentrantLock();
 
     static {
 
@@ -39,36 +46,63 @@ public class LocalCacheProviderImpl implements CacheProviderService {
 
     @Override
     public <T> T get(String key) {
-        return null;
+        return get(key, null, null, CACHE_MINUTE);
     }
 
     @Override
     public <T> T get(String key, Function<String, T> function) {
-        return null;
+        return get(key, function, null, CACHE_MINUTE);
     }
 
     @Override
     public <T, M> T get(String key, Function<M, T> function, M funcParm) {
-        return null;
+        return get(key, function, funcParm, CACHE_MINUTE);
     }
 
     @Override
     public <T> T get(String key, Function<String, T> function, Long expireTime) {
-        return null;
+        return get(key, function, null, expireTime);
     }
 
     @Override
     public <T, M> T get(String key, Function<M, T> function, M funcParm, Long expireTime) {
-        return null;
+        T obj = null;
+        if (StringUtils.isEmpty(key)) {
+            return obj;
+        }
+
+        expireTime = getExpireTime(expireTime);
+        Cache<String, Object> cacheContainer = getCacheContainer(expireTime);
+        if(ObjectUtils.isEmpty(function)){
+            obj = (T)cacheContainer.getIfPresent(key);
+        }else {
+            try {
+                obj = (T)cacheContainer.get(key,() ->{
+                    T retObj = function.apply(funcParm);
+                    return retObj;
+                });
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        return obj;
     }
 
     @Override
     public <T> void set(String key, T obj) {
-
+        set(key,obj,CACHE_MINUTE);
     }
 
     @Override
     public <T> void set(String key, T obj, Long expireTime) {
+        if(ObjectUtils.isEmpty(key) || ObjectUtils.isEmpty(obj)){
+            return;
+        }
+
+        expireTime = getExpireTime(expireTime);
+        Cache<String,Object> cacheContainer = getCacheContainer(expireTime);
+        cacheContainer.put(key,obj);
+
 
     }
 
@@ -82,8 +116,35 @@ public class LocalCacheProviderImpl implements CacheProviderService {
         return false;
     }
 
-    private Long getExpireTime(Long expireTime){
+    private Long getExpireTime(Long expireTime) {
 
         return expireTime;
+    }
+
+    private Cache<String, Object> getCacheContainer(Long expireTime) {
+        Cache<String, Object> cacheContainer = null;
+        if (ObjectUtils.isEmpty(expireTime)) {
+            return cacheContainer;
+        }
+
+        String mapKey = String.valueOf(expireTime);
+        if (_cacheMap.containsKey(mapKey)) {
+            cacheContainer = _cacheMap.get(mapKey);
+            return cacheContainer;
+        }
+
+        try {
+            lock.lock();
+            cacheContainer = CacheBuilder
+                    .newBuilder()
+                    .maximumSize(CACHE_MAXIMUM_SIZE)
+                    .expireAfterWrite(expireTime, TimeUnit.MICROSECONDS)
+                    .recordStats()
+                    .build();
+            _cacheMap.put(mapKey, cacheContainer);
+        } finally {
+            lock.unlock();
+        }
+        return cacheContainer;
     }
 }
