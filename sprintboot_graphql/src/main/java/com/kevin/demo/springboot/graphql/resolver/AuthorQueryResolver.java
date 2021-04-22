9450a434498b37e80ff600d9705932e604529efe
alvinl <alvinl@synnex.com>
2021-04-22 17:03:39 +0800

    graphql enhance

--- sprintboot_graphql/src/main/java/com/kevin/demo/springboot/graphql/resolver/AuthorQueryResolver.java
+++ sprintboot_graphql/src/main/java/com/kevin/demo/springboot/graphql/resolver/AuthorQueryResolver.java
@@ -3,6 +3,7 @@ package com.kevin.demo.springboot.graphql.resolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.kevin.demo.springboot.graphql.bo.Author;
 import com.kevin.demo.springboot.graphql.bo.Book;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
  * @author alvinl
  * @date 04/12/21 17:00
  **/
+@Component
 public class AuthorQueryResolver implements GraphQLQueryResolver {
     public List<Author> findAllAuthors() {
         Author author = new Author("scala编程第三版", "电子工业出版社");
